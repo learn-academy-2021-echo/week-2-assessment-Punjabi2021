3e3dd246d1b97edf3c2204d3aba2bc2a389b41a0
Punjabi2021 <93889575+Punjabi2021@users.noreply.github.com>
2021-12-13 00:20:58 -0800

    completed assessment

--- coding-concepts.js
+++ coding-concepts.js
@@ -1,73 +1,73 @@
-// ASSESSMENT 2: Coding Conceptual Questions
-
-// Examine the following examples.
-
-// First, predict the outcome based on your understanding of the code.
-// Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
-
-// --------------------1) What will this log?
-
-var cohort = "Echo 2021"
-// console.log(cohort.split(""))
-
-// a) Your answer:
-// b) Verify and explain:
-
-
-// --------------------2) What will this log?
-
-const greeter = (name) => {
-  `Hello, ${name}!`
-}
-// console.log(greeter("LEARN Student"))
-
-// a) Your answer:
-// b) Verify and explain:
-
-
-// --------------------3) What will this log?
-
-var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
-
-// a) Your answer:
-// b) Verify and explain:
-
-
-// --------------------4) What will this log?
-
-var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
-// console.log(onlyOdds)
-
-// a) Your answer:
-// b) Verify and explain:
-
-
-// --------------------5) What will this log?
-
-var myCodingSkills = {
-  languages: ["JavaScript", "Ruby"],
-  frameworks: ["React", "Ruby on Rails"],
-  databases: "PostgreSQL",
-  versionControl: "GitHub"
-}
-// console.log(myCodingSkills.languages[0])
-
-// a) Your answer:
-// b) Verify and explain:
-
-
-// --------------------STRETCH: What will this log?
-
-class Learn {
-  constructor(name){
-    this.student = name,
-    this.cohort = "Delta",
-    this.year = 2021
-  }
-}
-var learnStudent = new Learn("George")
-// console.log(learnStudent)
-
-// a) Your answer:
-// b) Verify and explain:
+// // ASSESSMENT 2: Coding Conceptual Questions
+//
+// // Examine the following examples.
+//
+// // First, predict the outcome based on your understanding of the code.
+// // Then, uncomment the console.log() and verify the output. Briefly explain why your initial answer was correct or incorrect.
+//
+// // --------------------1) What will this log?
+//
+// var cohort = "Echo 2021"
+// // console.log(cohort.split(""))
+//
+// // a) Your answer: Every letter and space between Echo and 2021 will be output in quotation marks.
+// // b) Verify and explain: the lack  of a space between the quotation marks sends the instruction to the terminal saying everthing inside of the quotation marks will be split into a pair ofquotation marks.
+//
+//
+// // --------------------2) What will this log?
+//
+ const greeter = (name) => {
+   `Hello, ${name}!`
+ }
+ console.log(greeter("LEARN Student"))
+//
+// a) Your answer: Hello (insert name) LEARN Student
+// // b) Verify and explain:Hello (name) LEARN Student
+//
+//
+// // --------------------3) What will this log?
+//
+// var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
+// // console.log(multBy2)
+//
+// // a) Your answer:8,10,12,14,16
+// // b) Verify and explain: I was correct. By using the high order function of map the code was able to take the array and multiply by two
+//
+//
+// // --------------------4) What will this log?
+//
+// var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
+// // console.log(onlyOdds)
+//
+// // a) Your answer: 11,13,15
+// // b) Verify and explain: var onlyOdds declare the value and the modulus operator identified of the number was divisble two and seprates the onlyOdds
+//
+//
+// // --------------------5) What will this log?
+//
+// var myCodingSkills = {
+//   languages: ["JavaScript", "Ruby"],
+//   frameworks: ["React", "Ruby on Rails"],
+//   databases: "PostgreSQL",
+//   versionControl: "GitHub"
+// }
+// // console.log(myCodingSkills.languages[0])
+//
+// // a) Your answer:JavaScript
+// // b) Verify and explain:the information is being pulled from languages-index zero value.
+//
+//
+// // --------------------STRETCH: What will this log?
+//
+// class Learn {
+//   constructor(name){
+//     this.student = name,
+//     this.cohort = "Delta",
+//     this.year = 2021
+//   }
+// }
+ var learnStudent = new Learn("George")
+ console.log(learnStudent)
+//
+// // a) Your answer:George is in Delta cohort 2021.
+// // b) Verify and explain: Learn { student: 'George', cohort: 'Delta', year: 2021 } the format inside of the curly bracktes stayed the same and wasnt turned into a sentance.
--- interview-questions.md
+++ interview-questions.md
@@ -8,49 +8,51 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-  Your answer:
+  Your answer:parameter has two points a min and a max,the argument is if this happens then do this (if the modulus operator of two is zero then the number is even)
 
-  Researched answer:
+  Researched answer:Parameters are variables listed as a part of the function definition. Arguments are values passed to the function when it is invoked.
 
 
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer:index and values, they are an array. the index is required the values optional.
 
-  Researched answer:
+  Researched answer:The Array object’s built-in map method takes a function definition as its first parameter.the callback function definition has a second parameter, the index.The map function has an often-overlooked optional second parameter. We can provide an object that will become ‘this’ within the scope of our callback.
 
 
 
 3. What is object destructuring?
 
-  Your answer:
+  Your answer:when giving the values its asignined
 
-  Researched answer:
+  Researched answer:The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+
+
 
 
 
 4. What is the difference between a function and a method?
 
-  Your answer:
+  Your answer:function is what something does method is the process of something being done
 
-  Researched answer:
+  Researched answer:a method is a function that belongs to a class. In JavaScript, however, a method is a function that belongs to an object.
 
 
 
 5. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer:objects (data) are sorted and arranged in a class (category)
 
-  Researched answer:
+  Researched answer:Class: A class is the building block that leads to Object-Oriented Programming. It is a user-defined data type, that holds its own data members and member functions, which can be accessed and used by creating an instance of that class. It is the blueprint of any object.
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: to lift code to another file.
 
-  Researched answer:
+  Researched answer:JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code. Hoisting allows functions to be safely used in code before they are declared.
 
 
 
@@ -58,12 +60,13 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. Class Inheritance:
+1. Class Inheritance:Inheritance is the procedure in which one class inherits the attributes and methods of another class. The class whose properties and methods are inherited is known as the Parent class.
 
-2. React:
+2. React:React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications
 
-3. React state:
+3. React state: allows us to manage changing data in an application. It's defined as an object where we define key-value pairs specifying various data we want to track in the application. In React, all the code we write is defined inside a component.
 
-4. React lifecycle methods:
+4. React lifecycle methods:Each component in React has a lifecycle which you can monitor and manipulate during its three main phases. The three phases are: Mounting, Updating, and Unmounting.
 
-5. DOM:
+
+5. DOM:The Document Object Model (DOM) is a programming interface for web documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects; that way, programming languages can interact with the page.
